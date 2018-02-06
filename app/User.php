7b69f74df663a5a20c08bd3f52035b4ec4af84d6
ゆうきゃん <35098175+ucan-lab@users.noreply.github.com>
2018-02-06 22:04:11 +0900

    #2 tasksのリレーション

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * 特定ユーザーの全タスク取得
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
