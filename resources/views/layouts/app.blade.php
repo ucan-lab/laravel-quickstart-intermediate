7fa86da01a531198978208428998f1cddd803aa3
ゆうきゃん <35098175+ucan-lab@users.noreply.github.com>
2018-02-06 22:55:42 +0900

    #4 レイアウトの定義

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Intermediate</title>
+
+        <!-- CSSJavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- ナビバーの内容 -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
