c453bc1d53ff441bd2c44791c84f6978984ca08f
ゆうきゃん <35098175+ucan-lab@users.noreply.github.com>
2018-02-12 03:01:19 +0900

    #6 削除ボタンの追加

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -57,8 +57,14 @@
                                     <div>{{ $task->name }}</div>
                                 </td>
 
+                                <!-- 削除ボタン -->
                                 <td>
-                                    <!-- TODO: 削除ボタン -->
+                                    <form action="/task/{{ $task->id }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <button>タスク削除</button>
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
