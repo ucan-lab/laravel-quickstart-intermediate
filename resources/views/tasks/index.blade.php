0909d2f16b61667c6c019dcb214e288084e2928e
ゆうきゃん <35098175+ucan-lab@users.noreply.github.com>
2018-02-12 02:37:51 +0900

    #5 タスク表示

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -32,5 +32,39 @@
         </form>
     </div>
 
-    <!-- TODO: 現在のタスク -->
+    <!-- 現在のタスク -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                現在のタスク
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- テーブルヘッダー -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- テーブルボディー -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <!-- タスク名 -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: 削除ボタン -->
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
